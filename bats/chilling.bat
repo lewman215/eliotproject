59a8a3694daa145b51c28241d460422af511194b
Your Name <eliotstidder@hotmail.com>
2020-12-22 17:43:22 +0000

    changed speed

--- bats/chilling.bat
+++ bats/chilling.bat
@@ -1,1 +1,1 @@
-cscript "C:\Users\Eliot-PC\Desktop\Jampal\ptts.vbs" -u "C:\Users\Eliot-PC\Desktop\hotkeystuff\textfiles\werechillign.txt"
+cscript "C:\Users\Eliot-PC\Desktop\Jampal\ptts.vbs" -u "C:\Users\Eliot-PC\Desktop\hotkeystuff\textfiles\werechillign.txt" -r -5
