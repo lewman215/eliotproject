18e41af2cda976c9fd3ec042aef4bdb395c43d45
Your Name <eliotstidder@hotmail.com>
2020-12-24 15:51:21 +0000

    rename jeff

---
+++ jeff.py
@@ -0,0 +1,6 @@
+import winsound 
+  
+print("Playing the file 'soundbites/jeff.wav'") 
+  
+# winsound.PlaySound('filename', flag) 
+winsound.PlaySound('soundbites/jeff.wav', winsound.SND_FILENAME)        
--- player.py
+++
@@ -1,6 +0,0 @@
-import winsound 
-  
-print("Playing the file 'soundbites\Welcome.wav'") 
-  
-# winsound.PlaySound('filename', flag) 
-winsound.PlaySound('jeff.wav', winsound.SND_FILENAME)        
